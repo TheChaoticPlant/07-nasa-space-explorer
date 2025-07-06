03b96b33c1c058aa9616dd399ba7f50d3ca5e360
Caitlyn Correa <cacorre4@email.asu.edu>
2025-07-06 03:59:21 +0000

    changes

--- js/script.js
+++ js/script.js
@@ -193,3 +193,4 @@ const endInput = document.getElementById('endDate');
 // - Default to a range of 9 days (from 9 days ago to today)
 // - Restrict dates to NASA's image archive (starting from 1995)
 setupDateInputs(startInput, endInput);
+  
